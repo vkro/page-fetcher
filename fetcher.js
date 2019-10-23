3de506c1bb35986d48259e2b2ce14fdc2cdcb980
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-23 23:31:30 +0000

    get command line args, pass url input to request

---
+++ fetcher.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+const args = process.argv.splice(2);
+
+
+const request = require('request');
+request(args[0], (error, response, body) => {
+  console.log('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log('body:', body); // Print the HTML for the Google homepage.
+});
