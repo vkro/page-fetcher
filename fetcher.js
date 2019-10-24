17c3d3a6400ba99af3c3a62a34dc8262b58bd94c
Vanessa Kroeker <vkroeker@gmail.com>
2019-10-24 00:45:09 +0000

    fetcher takes url and local file path, downloads resource to path

--- fetcher.js
+++ fetcher.js
@@ -1,11 +1,28 @@
 const fs = require('fs');
-
 const args = process.argv.splice(2);
-
-
 const request = require('request');
-request(args[0], (error, response, body) => {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+
+
+// Takes a URL and local file path as command line args
+// Downloads the resource to the specified path
+
+const fetcher = function (args) {
+  const url = args[0];
+  const path = args[1]
+
+  request(url, path, (error, response, body) => {
+    fs.writeFile(path, body, () => {
+      fs.stat(path, (error, stat) => {
+      process.stdout.write(`Downloaded and saved ${stat.size} bytes to ${path}\n`)
+      })
+    })
+  })
+};
+
+// request(args[0], (error, response, body) => {
+//   console.log('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+fetcher(args);
